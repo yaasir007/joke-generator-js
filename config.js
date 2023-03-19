928b3eb006115ddb18f498c1a5ccaf45cde553c4
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-19 23:29:52 +0400

    working on logic

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-  RAPID_DAD_JOKE_API: "https://dad-jokes.p.rapidapi.com/random/joke",
+  RAPID_DAD_JOKE_API: "https://joke3.p.rapidapi.com/v1/joke",
   RAPID_DAD_JOKE_API_KEY: "1da92de020msh88608772bd7f708p16ff04jsnc92b20273f45",
-  RAPID_HOST: "dad-jokes.p.rapidapi.com",
+  RAPID_HOST: "joke3.p.rapidapi.com",
 };
