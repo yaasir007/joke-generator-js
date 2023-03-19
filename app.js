4f8fad4d8707fee11da53aa84300a0fc20a7ea6c
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-19 23:37:55 +0400

    removed api key

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-let api_key = config.RAPID_DAD_JOKE_API_KEY;
-let api_link = config.RAPID_DAD_JOKE_API;
-let api_host = config.RAPID_HOST;
+// let api_key = config.RAPID_DAD_JOKE_API_KEY;
+// let api_link = config.RAPID_DAD_JOKE_API;
+// let api_host = config.RAPID_HOST;
 
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "1da92de020msh88608772bd7f708p16ff04jsnc92b20273f45",
-//     "X-RapidAPI-Host": "myallies-breaking-news-v1.p.rapidapi.com",
-//   },
-// };
-
-// fetch("https://myallies-breaking-news-v1.p.rapidapi.com/news/cnn", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "1da92de020msh88608772bd7f708p16ff04jsnc92b20273f45",
-    "X-RapidAPI-Host": "cycoshas-mouth-talking-app-v1.p.rapidapi.com",
-  },
-};
-
-fetch(
-  "https://cycoshas-mouth-talking-app-v1.p.rapidapi.com/best-mouth-talking-apps/",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
