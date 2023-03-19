2202fa6b7df65bffb6ea646b528dc88522f88c15
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-19 23:38:40 +0400

    removed

--- config.js
+++
@@ -1,5 +0,0 @@
-let config = {
-  RAPID_DAD_JOKE_API: "https://joke3.p.rapidapi.com/v1/joke",
-  RAPID_DAD_JOKE_API_KEY: "1da92de020msh88608772bd7f708p16ff04jsnc92b20273f45",
-  RAPID_HOST: "joke3.p.rapidapi.com",
-};
